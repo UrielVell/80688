ca690e8315af59fce49b89a8ee58625c53236752
UrielVell <urielvh84@gmail.com>
2023-10-12 15:05:36 -0600

    Actividad 08

---
+++ practica08/src/MiFieldSet.jsx
@@ -0,0 +1,18 @@
+function MiFieldSet(props){
+    const {titulo, input1, input2} = props
+    
+    return (
+    <>
+        <fieldset>
+            <legend>{titulo}</legend>
+            <label htmlFor={input1}>{input1} </label>
+            <input type="text" id={input1} />
+            <label htmlFor={input2}>{input2} </label>
+            <input type="text" id={input2} />
+        </fieldset>
+    </>
+    )
+}
+
+
+export default MiFieldSet
